4431b5d6a812067fc2686e2dda25d9ee4792ee96
NikHram <whramnik@gmail.com>
2022-06-07 14:40:46 +0200

    First push

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculate() {
+    document.getElementById("answer").innerHTML = Number(document.getElementById("number1").value) + Number(document.getElementById("number2").value);
+    setTimeout(() => {  document.getElementById("number1").setAttribute("disabled", "disabled"); }, 1500);
+    setTimeout(() => {  document.getElementById("number2").setAttribute("disabled", "disabled"); }, 2200);
+    setTimeout(() => {  document.getElementById("Prikol").innerHTML = "You broke everything!" }, 3000);
+    setTimeout(() => {  document.getElementById("Prikol").innerHTML = "Never do math again." }, 5000);
+}
+
