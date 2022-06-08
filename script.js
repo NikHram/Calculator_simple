4116d840d456220ab71ac40a1a4132a16a0e6e8b
NikHram <whramnik@gmail.com>
2022-06-08 11:23:01 +0200

    Centrification Flex fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calculate() {
     document.getElementById("answer").innerHTML = Number(document.getElementById("number1").value) + Number(document.getElementById("number2").value);
     setTimeout(() => {  document.getElementById("number1").setAttribute("disabled", "disabled"); }, 1500);
     setTimeout(() => {  document.getElementById("number2").setAttribute("disabled", "disabled"); }, 2200);
-    setTimeout(() => {  document.getElementById("Prikol").innerHTML = "You broke everything!" }, 3000);
-    setTimeout(() => {  document.getElementById("Prikol").innerHTML = "Never do math again." }, 5000);
+    setTimeout(() => {  document.getElementById("joke").innerHTML = "You broke everything!" }, 3000);
+    setTimeout(() => {  document.getElementById("joke").innerHTML = "Never do math again." }, 5000);
 }
 
