261bc8f78e8d6eb7e7b5ae4e62324ebcf594ad8a
NikHram <whramnik@gmail.com>
2022-06-09 20:33:18 +0200

    Just cause

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calculate() {
-    document.getElementById("answer").innerHTML = Number(document.getElementById("number1").value) + Number(document.getElementById("number2").value);
+    document.getElementById("answer").innerHTML = +document.getElementById("number1").value + +document.getElementById("number2").value;
     setTimeout(() => {  document.getElementById("number1").setAttribute("disabled", "disabled"); }, 1500);
     setTimeout(() => {  document.getElementById("number2").setAttribute("disabled", "disabled"); }, 2200);
     setTimeout(() => {  document.getElementById("joke").innerHTML = "You broke everything!" }, 3000);
